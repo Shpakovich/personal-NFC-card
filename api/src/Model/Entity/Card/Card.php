8b5e16e726b3a0ad2f56eea56e3fdc4ebfc6dc71
Vlad Duplin <v.duplin@rabota.ru>
2021-07-12 21:30:46 +0300

    Add the card entity

---
+++ api/src/Model/Entity/Card/Card.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Entity\Card;
+
+use App\Model\Entity\User\User;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="cards")
+ */
+class Card
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="card_id")
+     */
+    private Id $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Model\Entity\User\User", cascade={"persist"})
+     * @ORM\JoinColumn(name="created_by", referencedColumnName="id", onDelete="RESTRICT", nullable=false)
+     */
+    private User $creator;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private \DateTimeImmutable $createdAt;
+
+    public function __construct(Id $id, User $creator, \DateTimeImmutable $createdAt)
+    {
+        $this->id = $id;
+        $this->creator = $creator;
+        $this->createdAt = $createdAt;
+    }
+
+    public function getId(): Id
+    {
+        return $this->id;
+    }
+
+    public function getCreator(): User
+    {
+        return $this->creator;
+    }
+
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+}
