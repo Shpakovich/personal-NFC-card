776fa3632465810acebae742c05baa748ae27140
d.shmakov <d.shmakov@rabota.ru>
2021-07-12 15:19:13 +1000

    frontend-auth+reg-style кастомная иконка глаза на инпуте пароля

--- frontend/components/userRegForm.vue
+++ frontend/components/userRegForm.vue
@@ -37,6 +37,12 @@
         passwordConfirmationRule() {
           return () =>
             this.userInfo.password === this.userInfo.confirmPassword || "Пароли не совпадают";
+        },
+        colorPasswordIcon () {
+          return this.showPassword ? '#68676C' : '#FFA436';
+        },
+        colorConfirmPasswordIcon () {
+          return this.showConfirm ? '#68676C' : '#FFA436';
         }
       }
     }
@@ -62,7 +68,6 @@
     <v-text-field
       class="font-croc"
       v-model="userInfo.password"
-      :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
       :rules="passwordRules"
       :type="showPassword ? 'text' : 'password'"
       name="password"
@@ -71,13 +76,19 @@
       hint="Пароль недёжный, наверное"
       outlined
       counter
-      @click:append="showPassword = !showPassword"
-    ></v-text-field>
+    >
+      <template v-slot:append>
+        <div @click="showPassword = !showPassword">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.12 14.12C13.8454 14.4147 13.5141 14.6512 13.1462 14.8151C12.7782 14.9791 12.3809 15.0673 11.9781 15.0744C11.5753 15.0815 11.1752 15.0074 10.8016 14.8565C10.4281 14.7056 10.0887 14.481 9.80385 14.1962C9.51897 13.9113 9.29439 13.5719 9.14351 13.1984C8.99262 12.8248 8.91853 12.4247 8.92563 12.0219C8.93274 11.6191 9.02091 11.2218 9.18488 10.8538C9.34884 10.4859 9.58525 10.1546 9.88 9.88M1 1L23 23M17.94 17.94C16.2306 19.243 14.1491 19.9649 12 20C5 20 1 12 1 12C2.24389 9.6819 3.96914 7.65661 6.06 6.06L17.94 17.94ZM9.9 4.24C10.5883 4.07888 11.2931 3.99834 12 4C19 4 23 12 23 12C22.393 13.1356 21.6691 14.2047 20.84 15.19L9.9 4.24Z" :stroke="colorPasswordIcon" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+        </div>
+      </template>
+    </v-text-field>
 
     <v-text-field
       class="font-croc"
       v-model="userInfo.confirmPassword"
-      :append-icon="showConfirm ? 'mdi-eye' : 'mdi-eye-off'"
       :rules="confirmPasswordRules.concat(passwordConfirmationRule)"
       :type="showConfirm ? 'text' : 'password'"
       name="password"
@@ -85,8 +96,15 @@
       placeholder="Ваш пароль"
       counter
       outlined
-      @click:append="showConfirm = !showConfirm"
-    ></v-text-field>
+    >
+      <template v-slot:append>
+        <div @click="showConfirm = !showConfirm">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.12 14.12C13.8454 14.4147 13.5141 14.6512 13.1462 14.8151C12.7782 14.9791 12.3809 15.0673 11.9781 15.0744C11.5753 15.0815 11.1752 15.0074 10.8016 14.8565C10.4281 14.7056 10.0887 14.481 9.80385 14.1962C9.51897 13.9113 9.29439 13.5719 9.14351 13.1984C8.99262 12.8248 8.91853 12.4247 8.92563 12.0219C8.93274 11.6191 9.02091 11.2218 9.18488 10.8538C9.34884 10.4859 9.58525 10.1546 9.88 9.88M1 1L23 23M17.94 17.94C16.2306 19.243 14.1491 19.9649 12 20C5 20 1 12 1 12C2.24389 9.6819 3.96914 7.65661 6.06 6.06L17.94 17.94ZM9.9 4.24C10.5883 4.07888 11.2931 3.99834 12 4C19 4 23 12 23 12C22.393 13.1356 21.6691 14.2047 20.84 15.19L9.9 4.24Z" :stroke="colorConfirmPasswordIcon" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+        </div>
+      </template>
+    </v-text-field>
     <div class="flex flex-row ml-4 mb-6">
       <input v-model="checkbox" :rules="checkboxRules" class="ml-4 font-croc custom-checkbox" type="checkbox" id="privacy" name="privacy">
       <label for="privacy">Я согласен(а) на обработку персональных данных и соглашаюсь<nuxt-link class="contents" to="/privacy">с политикой конфиденциальности</nuxt-link>
