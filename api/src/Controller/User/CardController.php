16c6164c076403bdbab5b88aeda7ab9467d6912a
Vlad Duplin <v.duplin@rabota.ru>
2021-08-13 22:41:21 +0300

    Add API to get a user card by card ID

--- api/src/Controller/User/CardController.php
+++ api/src/Controller/User/CardController.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace App\Controller\User;
 
+use App\Controller\Guid;
 use App\Controller\PaginationSerializer;
 use App\Fetcher\User;
 use App\Model\Entity\User\Role;
+use App\Model\Entity\User\UserCard;
+use App\Security\Voter\User\UserCardAccess;
+use DateTimeInterface;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use Nelmio\ApiDocBundle\Annotation\Security;
 use OpenApi\Annotations as OA;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -95,6 +100,67 @@ class CardController extends AbstractController
                     (array)$pagination->getItems()
                 ),
                 'pagination' => PaginationSerializer::serialize($pagination)
+            ]
+        );
+    }
+
+    /**
+     * @Route("/{card_id}", methods={"GET"}, name=".show", requirements={
+     *     "id"=Guid::PATTERN
+     * })
+     * @ParamConverter("card", options={"mapping": {"card_id": "card"}})
+     *
+     * @OA\Get(
+     *     summary="Получить зарегистрированную карту пользователя по ее ID"
+     * )
+     *
+     * @OA\Parameter(
+     *     name="card_id",
+     *     in="path",
+     *     description="ID карты",
+     *     required=true,
+     *     @OA\Schema(type="string")
+     * )
+     *
+     * @OA\Response(
+     *     response=200,
+     *     description="OK",
+     *     @OA\JsonContent(
+     *         @OA\Property(property="id", type="string", description="ID"),
+     *         @OA\Property(property="card_id", type="string", description="ID карты"),
+     *         @OA\Property(property="alias", type="string", description="Псевдоним карты"),
+     *         @OA\Property(property="added_at", type="string", description="Дата добавления"),
+     *         @OA\Property(property="user", type="object", description="Пользователь",
+     *             @OA\Property(property="id", type="string", description="ID"),
+     *             @OA\Property(property="email", type="string", description="Email")
+     *         )
+     *     )
+     * )
+     *
+     * @OA\Response(response=404, description="Не найдена")
+     * @OA\Response(response=401, description="Требуется авторизация")
+     * @OA\Response(response=403, description="Доступ запрещен")
+     *
+     * @OA\Tag(name="User")
+     * @Security(name="Bearer")
+     *
+     * @param \App\Model\Entity\User\UserCard $card
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function show(UserCard $card): JsonResponse
+    {
+        $this->denyAccessUnlessGranted(UserCardAccess::VIEW, $card);
+
+        return $this->json(
+            [
+                'id' => $card->getId()->getValue(),
+                'card_id' => $card->getCard()->getId()->getValue(),
+                'alias' => $card->getAlias(),
+                'added_at' => $card->getAddedAt()->format(DateTimeInterface::RFC3339),
+                'user' => [
+                    'id' => $card->getUser()->getId()->getValue(),
+                    'email' => $card->getUser()->getEmail()->getValue(),
+                ]
             ]
         );
     }
