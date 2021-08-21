24c727a29ec811dbae43dbd6f144b532965031d1
d.shmakov <d.shmakov@rabota.ru>
2021-08-21 18:45:41 +0300

    frontend-profile_component+add_fields если мы авторизованы, то ходим на получение профиля

--- frontend/store/actions.js
+++ frontend/store/actions.js
@@ -1,9 +1,11 @@
 const actions = {
     async nuxtServerInit ({ dispatch }) {
-        await Promise.all([
-            // dispatch('profile/getAllProfilesInfo'),
-            // TODO получать инфу по юзеру /user/cards
-        ]);
+        if (this.$auth.loggedIn) {
+            await Promise.all([
+                dispatch('profile/getAllProfilesInfo'),
+                // TODO получать инфу по юзеру /user/cards
+            ]);
+        }
     }
 };
 
