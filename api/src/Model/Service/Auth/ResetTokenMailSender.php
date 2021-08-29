adcc8f55fb5639ffa49d42ef817bcbc0cac57bef
Vlad Duplin <v.duplin@rabota.ru>
2021-08-29 14:55:20 +0300

    Change a template to the reset password

--- api/src/Model/Service/Auth/ResetTokenMailSender.php
+++ api/src/Model/Service/Auth/ResetTokenMailSender.php
@@ -6,25 +6,30 @@ namespace App\Model\Service\Auth;
 
 use App\Model\Entity\User\Email;
 use App\Model\Entity\User\Token;
-use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
+use Twig\Environment;
 
 class ResetTokenMailSender
 {
     private MailerInterface $mailer;
+    private Environment $twig;
 
-    public function __construct(MailerInterface $mailer)
+    public function __construct(MailerInterface $mailer, Environment $twig)
     {
         $this->mailer = $mailer;
+        $this->twig = $twig;
     }
 
     public function send(Email $email, Token $token): void
     {
+        $html = $this->twig->render('mail/user/reset.html.twig', [
+            'token' => $token->getValue()
+        ]);
+
         $message = (new \Symfony\Component\Mime\Email())
             ->to($email->getValue())
-            ->subject('Reset token')
-            ->text('Reset token: ' . $token->getValue())
-            ->html('<p>Reset token: ' . $token->getValue() . '</p>');
+            ->subject('Запрос на изменение пароля')
+            ->html($html);
 
         $this->mailer->send($message);
     }
