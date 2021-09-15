871128258698e1fa1cfcac54232a216025d1e44e
d.shmakov <d.shmakov@rabota.ru>
2021-09-15 14:27:14 +0300

    frontend-reliz_fix3 доработка филдов Вайбера

--- frontend/pages/profile/fields/addField.vue
+++ frontend/pages/profile/fields/addField.vue
@@ -15,6 +15,7 @@
             fieldValue: '',
             loading: false,
             valid: false,
+            isContactViber: true,
             valueRules: [
                 v => !!v || 'Поле не должно быть пустым'
             ],
@@ -34,6 +35,9 @@
 
             getPlaceholder () {
                 return 'Ввведите ' + this.filedInfo?.title;
+            },
+            isViber () {
+                return this.filedInfo.title === 'Viber';
             }
         },
 
@@ -75,6 +79,10 @@
             getIconSrc (fieldInfo) {
                 return fieldInfo?.icon?.path;
             },
+            changeContactStatus() {
+                this.isContactViber = !this.isContactViber;
+                this.createFieldMask();
+            },
             createFieldMask() {
                 switch (this.filedInfo.title) {
                     case 'Номер телефона': {
@@ -102,6 +110,16 @@
                     case 'VK': {
                         this.placeholder = this.getPlaceholder;
                         return this.mask = 'https://vk.com/' + this.maskContinue
+                    }
+                    case 'Viber': {
+                        this.placeholder = this.getPlaceholder;
+                        if (this.isContactViber) {
+                            this.placeholder = '+7 (999) 999-99-99';
+                            return this.mask = '+# (###) ###-##-##';
+                        } else {
+                            this.placeholder = 'https://chats.viber.com/myid-card';
+                            return this.mask = 'https://chats.viber.com/' + this.maskContinue;
+                        }
                     }
                     case 'Linkedin': {
                         this.placeholder = this.getPlaceholder;
@@ -168,10 +186,6 @@
 <template>
     <v-container v-if="filedInfo" class="py-11 px-11">
         <h3 style="font-size: 24px; line-height: 35px;" class="text-center font-bold font-croc mb-2">{{ filedInfo.title }}</h3>
-        <!--<p class="text-center text-sm font-croc mb-2">Укажи ссылку, начиная с https<br/>(например,
-            <a href="https://Instagram.com/crocinc" target="_blank">
-                https://Instagram.com/crocinc
-            </a>)</p> -->
         <v-btn
                 icon
                 class="rounded-lg flex-initial font-bold w-4/12 mb-3 ml-1.5 btn-back"
@@ -203,6 +217,13 @@
                 class="flex flex-col mt-14"
                 v-model="valid"
         >
+            <div v-if="isViber" class="flex flex-row justify-around ml-4 mb-6">
+                <input @click="changeContactStatus" :checked="isContactViber" class="ml-4 font-croc custom-checkbox" type="radio" id="name" name="privacy">
+                <label for="name">Контакт</label>
+                <input @click="changeContactStatus" :checked="!isContactViber" class="ml-4 font-croc custom-checkbox" type="radio" id="nickname" name="privacy">
+                <label for="nickname">Чат/Группа</label>
+            </div>
+
             <v-text-field
                     v-model="fieldValue"
                     v-mask="mask"
@@ -230,6 +251,51 @@
     </v-container>
 </template>
 
-<style scoped>
+<style lang="scss">
+    .custom-checkbox {
+        position: absolute;
+        z-index: -1;
+        opacity: 0;
+    }
 
+    .custom-checkbox+label {
+        display: inline-flex;
+        align-items: center;
+        user-select: none;
+    }
+    .custom-checkbox+label::before {
+        content: '';
+        display: inline-block;
+        width: 1.5rem;
+        height: 1.5rem;
+        flex-shrink: 0;
+        flex-grow: 0;
+        border: 1px solid $secondary;
+        border-radius: 4px;
+        margin-right: 0.5rem;
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-size: 50% 50%;
+    }
+
+    .custom-checkbox:checked+label::before {
+        border-color: $secondary;
+        background-color: $secondary;
+        filter: drop-shadow(0px 4px 8px rgba(50, 50, 71, 0.16));
+        background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%23fff' d='M6.564.75l-3.59 3.612-1.538-1.55L0 4.26 2.974 7.25 8 2.193z'/%3e%3c/svg%3e");
+    }
+
+    label {
+        font-size: 12px;
+        line-height: 20px;
+        color: #68676C;
+    }
+
+    .v-input__slot {
+        min-height: 50px!important;
+    }
+
+    .v-text-field input{
+        padding: 12px 0 0 !important;
+    }
 </style>
