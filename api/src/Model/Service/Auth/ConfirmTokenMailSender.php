9b827b01993c8c851327df9623c182502a468d8b
Vlad Duplin <v.duplin@rabota.ru>
2021-09-21 09:51:06 +0300

    Add email template of signup

--- api/src/Model/Service/Auth/ConfirmTokenMailSender.php
+++ api/src/Model/Service/Auth/ConfirmTokenMailSender.php
@@ -6,25 +6,30 @@ namespace App\Model\Service\Auth;
 
 use App\Model\Entity\User\Email;
 use App\Model\Entity\User\Token;
-use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
+use Twig\Environment;
 
 class ConfirmTokenMailSender
 {
     private MailerInterface $mailer;
+    private Environment $twig;
 
-    public function __construct(MailerInterface $mailer)
+    public function __construct(MailerInterface $mailer, Environment $twig)
     {
         $this->mailer = $mailer;
+        $this->twig = $twig;
     }
 
     public function send(Email $email, Token $token): void
     {
+        $html = $this->twig->render('mail/user/signup.html.twig', [
+            'token' => $token->getValue()
+        ]);
+
         $message = (new \Symfony\Component\Mime\Email())
             ->to($email->getValue())
-            ->subject('Confirm email')
-            ->text('Confirm token: ' . $token->getValue())
-            ->html('<p>Confirm token: ' . $token->getValue() . '</p>');
+            ->subject('Подтверждение регистрации')
+            ->html($html);
 
         $this->mailer->send($message);
     }
