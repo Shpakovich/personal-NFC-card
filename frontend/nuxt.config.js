964bf67d37e3605988e017e947a5e5f8d862066e
d.shmakov <d.shmakov@rabota.ru>
2021-12-28 11:42:28 +0300

    frontend-SEO_styles поменял title страничек добавил head и og image

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -6,15 +6,14 @@ export const apiServerEndpoint = process.env.API_ENDPOINT_SERVER;
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: 'personal-NFC-card',
-    title: 'personal-NFC-card',
+    title: 'myID электронная визитка',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/myID-favicon.ico' }
